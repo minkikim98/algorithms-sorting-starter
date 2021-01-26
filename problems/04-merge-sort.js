640c5b19d76c4b2813e4b110c8a8858b51a119fc
minkikim98 <minkikim98@gmail.com>
2021-01-26 12:38:23 -0800

    finished mergesort; accounted for empty array

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,49 +1,37 @@
 // Implement Merge Sort
 
 function merge(array1, array2) {
-    let newArr = [];
-    let arrIdx1 = 0;
-    let arrIdx2 = 0;
-    while (arrIdx1 < array1.length || arrIdx2 < array2.length){
-      if (arrIdx1 >= array1.length ){
-        newArr.push(array2[arrIdx2]);
-        arrIdx2++;
-
-      } else if (array1[arrIdx1] > array2[arrIdx2]){
-        newArr.push(array2[arrIdx2]);
-        arrIdx2++;
-      } else if (arrIdx2 >= array2.length){
-        newArr.push(array1[arrIdx1]);
-        arrIdx1++;
-
-      } else if (array2[arrIdx2] >= array1[arrIdx1]){
-        newArr.push(array1[arrIdx1]);
-        arrIdx1++;
-      }
-      
-      
-      
+  let newArr = [];
+  let arrIdx1 = 0;
+  let arrIdx2 = 0;
+  while (arrIdx1 < array1.length || arrIdx2 < array2.length) {
+    if (arrIdx1 >= array1.length) {
+      newArr.push(array2[arrIdx2]);
+      arrIdx2++;
+    } else if (array1[arrIdx1] > array2[arrIdx2]) {
+      newArr.push(array2[arrIdx2]);
+      arrIdx2++;
+    } else if (arrIdx2 >= array2.length) {
+      newArr.push(array1[arrIdx1]);
+      arrIdx1++;
+    } else if (array2[arrIdx2] >= array1[arrIdx1]) {
+      newArr.push(array1[arrIdx1]);
+      arrIdx1++;
     }
-    return newArr;
+  }
+  return newArr;
 }
 
 function mergeSort(array) {
-    if (array.length === 0){
-      return array;
-    }
-    if (array.length === 1){
-      return array;
-    }
-    let midpoint = Math.floor(array.length/2);
-    console.log(midpoint, array.length);
-    let left = array.slice(0, midpoint);
-    let right = array.slice(midpoint, array.length);
+  if (array.length === 0 || array.length === 1) return array;
+  let midpoint = Math.floor(array.length / 2);
+  let left = array.slice(0, midpoint);
+  let right = array.slice(midpoint, array.length);
 
-    let leftSorted = mergeSort(left);
-    let rightSorted = mergeSort(right);
+  let leftSorted = mergeSort(left);
+  let rightSorted = mergeSort(right);
 
-    return merge(leftSorted, rightSorted);
-
+  return merge(leftSorted, rightSorted);
 }
 
 module.exports = {
