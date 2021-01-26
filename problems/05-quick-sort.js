a50929aaaa6183866d9215bd615ae8b7beb9c062
Savannah Rice <savannah.rice@gmail.com>
2021-01-26 14:10:24 -0700

    Finished quick sort

--- problems/05-quick-sort.js
+++ problems/05-quick-sort.js
@@ -3,21 +3,24 @@
 function quickSort(array) {
   if (array.length === 1 || array.length === 0) return array;
   let pivot = array[0];
+
   let newArr = partition(array, pivot);
-  return quickSort(newArr[0]).concat(quickSort(newArr[1]));
+  let lowSorted = quickSort(newArr[0]);
+  let highSorted = quickSort(newArr[1]);
+  return [...lowSorted, pivot, ...highSorted];
 }
 
 function partition(array, pivot) {
   let highArr = [];
   let lowArr = [];
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 1; i < array.length; i++) {
     if (array[i] >= pivot) highArr.push(array[i]);
     else if (array[i] < pivot) lowArr.push(array[i]);
   }
-  return [ lowArr, highArr ];
+
+  return [lowArr, highArr];
 }
 
-
 module.exports = {
-  quickSort
+  quickSort,
 };
