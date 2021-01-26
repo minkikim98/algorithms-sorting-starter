df1eae0ac90639168139341bc4943afd3c73dec3
Savannah Rice <savannah.rice@gmail.com>
2021-01-26 13:30:26 -0700

    Almost finished merge sort

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,11 +1,49 @@
 // Implement Merge Sort
 
 function merge(array1, array2) {
-  // your code here
+    let newArr = [];
+    let arrIdx1 = 0;
+    let arrIdx2 = 0;
+    while (arrIdx1 < array1.length || arrIdx2 < array2.length){
+      if (arrIdx1 >= array1.length ){
+        newArr.push(array2[arrIdx2]);
+        arrIdx2++;
+
+      } else if (array1[arrIdx1] > array2[arrIdx2]){
+        newArr.push(array2[arrIdx2]);
+        arrIdx2++;
+      } else if (arrIdx2 >= array2.length){
+        newArr.push(array1[arrIdx1]);
+        arrIdx1++;
+
+      } else if (array2[arrIdx2] >= array1[arrIdx1]){
+        newArr.push(array1[arrIdx1]);
+        arrIdx1++;
+      }
+      
+      
+      
+    }
+    return newArr;
 }
 
 function mergeSort(array) {
-  // your code here
+    if (array.length === 0){
+      return array;
+    }
+    if (array.length === 1){
+      return array;
+    }
+    let midpoint = Math.floor(array.length/2);
+    console.log(midpoint, array.length);
+    let left = array.slice(0, midpoint);
+    let right = array.slice(midpoint, array.length);
+
+    let leftSorted = mergeSort(left);
+    let rightSorted = mergeSort(right);
+
+    return merge(leftSorted, rightSorted);
+
 }
 
 module.exports = {
