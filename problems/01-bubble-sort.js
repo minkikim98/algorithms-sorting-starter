20bc30f8254099e7f2a43ed3556cb7649b4eb151
minkikim98 <minkikim98@gmail.com>
2021-01-26 09:49:17 -0800

    finished bubble sort

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -8,6 +8,18 @@ function swap(array, idx1, idx2) {
 
 function bubbleSort(array) {
   // your code here
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 1; i < array.length; i++) {
+      let idx1 = i - 1;
+      let idx2 = i;
+      if (array[idx2] < array[idx1]) {
+        swap(array, idx1, idx2);
+        swapped = true;
+      }
+    }
+  }
 }
 
 module.exports = {
