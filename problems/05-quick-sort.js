06a925a57a36d598eb7e6f1eae8bcccfb5b51f87
minkikim98 <minkikim98@gmail.com>
2021-01-26 12:59:42 -0800

    working on quicksort

--- problems/05-quick-sort.js
+++ problems/05-quick-sort.js
@@ -1,8 +1,20 @@
 // Implement Quick Sort
 
 function quickSort(array) {
-  
+  if (array.length === 1 || array.length === 0) return array;
+  let pivot = array[0];
+  let newArr = partition(array, pivot);
+  return quickSort(newArr[0]).concat(quickSort(newArr[1]));
+}
 
+function partition(array, pivot) {
+  let highArr = [];
+  let lowArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= pivot) highArr.push(array[i]);
+    else if (array[i] < pivot) lowArr.push(array[i]);
+  }
+  return [ lowArr, highArr ];
 }
 
 
